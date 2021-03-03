50270059124f01690f2e4709f183e0a2b124b737
baimard <benjamin@cybercorp.fr>
2021-03-03 14:31:30 +0100

    Integration webpack

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,35 @@
+import '../css/mycss.less';
+import 'datatables.net-dt/css/jquery.dataTables.css';
+
+import {generateUrl} from "@nextcloud/router";
+var baseUrl = generateUrl('/apps/gestion');
+
+
+import $ from 'jquery';
+import dt from 'datatables.net';
+
+
 $(window).on("load", function(){
-    console.log("webpackok");
+
+    $.ajax({
+        url: baseUrl+'/getClients',
+        type: 'PROPFIND',
+        contentType: 'application/json'
+    }).done(function (response) {
+        $.each(JSON.parse(response), function(arrayID, myresp) {
+           $('#client').DataTable().row.add([myresp.id,
+                                            myresp.entreprise,
+                                            myresp.nom,
+                                            myresp.prenom,
+                                            myresp.siret,
+                                            myresp.telephone,
+                                            myresp.mail,
+                                            myresp.adresse]);
+           //$('#client').DataTable().row.add(myresp);
+        });
+        $('#client').DataTable().draw(false);
+    }).fail(function (response, code) {
+        console.log(code);
+    });
+    //console.log("webpackok");
   });

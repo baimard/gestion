46ce743ff98e5fee6230377b8c85c1d24cdca769
baimard <benjamin@cybercorp.fr>
2021-03-03 19:41:40 +0100

    Webpack upgrade

--- js/scriptproduit.js
+++ js/scriptproduit.js
@@ -1,17 +1,5 @@
 var baseUrl = OC.generateUrl('/apps/gestion');
 
 $(document).ready( function () {
-    $.ajax({
-        url: baseUrl+'/getProduits',
-        type: 'PROPFIND',
-        contentType: 'application/json'
-    }).done(function (response) {
-        $.each(JSON.parse(response), function(arrayID, myresp) {
-           $('#produit').DataTable().row.add([myresp.id,myresp.reference,myresp.description]);
-           //$('#client').DataTable().row.add(myresp);
-        });
-        $('#produit').DataTable().draw(false);
-    }).fail(function (response, code) {
-        console.log(code);
-    });
+
 } );

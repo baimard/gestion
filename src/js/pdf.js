a7c2b70439da3dc4317c148847c9ab4c4a179875
Valdnet <47037905+Valdnet@users.noreply.github.com>
2021-11-30 10:41:18 +0100

    l10n: Correct spelling
    Translator's report from Transifex.

--- src/js/pdf.js
+++ src/js/pdf.js
@@ -26,7 +26,7 @@ var sendMail = function(myData){
     contentType: 'application/json',
     data: JSON.stringify(myData)
   }).done(function (response) {
-    showMessage(t('gestion', 'Mail sended'));
+    showMessage(t('gestion', 'Mail sent'));
   }).fail(function (response, code) {
     showMessage(t('gestion', 'There is an error'));
     error(response);

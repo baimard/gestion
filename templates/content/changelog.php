74a03aa61c68857a2fc0a1c8c6860d96b95c9ee6
Valdnet <47037905+Valdnet@users.noreply.github.com>
2022-01-10 12:44:21 +0100

    l10n: Correct spelling

--- templates/content/changelog.php
+++ templates/content/changelog.php
@@ -8,7 +8,7 @@
 		<p style="margin-bottom:20px;"><?php p($l->t('If you need documentation, follow this link')); ?> &#128073; <a href="https://baimard.github.io/gestion/"><?php p($l->t('Documentation')); ?></a></p>
 		<p style="margin-bottom:20px;"><?php p($l->t('Others questions?')); ?> &#128073; <a href="mailto:contact@cybercorp.fr"><?php p($l->t('Contact')); ?></a></p>
 		<p style="margin-bottom:20px;"><?php p($l->t('Leave me a comment, but only if you like this application :)')); ?> &#128073; <a href="https://apps.nextcloud.com/apps/gestion"><?php p($l->t('Nextcloud apps')); ?></a></p>
-		<p style="margin-bottom:20px;"><?php p($l->t('Want to talk with the community ?')); ?> &#128073; <a href="https://github.com/baimard/gestion/discussions"><?php p($l->t('Git discussion')); ?></a></p>
+		<p style="margin-bottom:20px;"><?php p($l->t('Want to talk with the community?')); ?> &#128073; <a href="https://github.com/baimard/gestion/discussions"><?php p($l->t('Git discussion')); ?></a></p>
 		<hr />
 		<h2><?php p($l->t('Changelog')); ?> v1.3.1.1</h2>
 		<p>

0c7aa3c507092996619f7c231037bb908d022e3d
Valdnet <47037905+Valdnet@users.noreply.github.com>
2022-02-14 08:56:23 +0100

    l10n: Change to plural (#162)

--- templates/navigation/index.php
+++ templates/navigation/index.php
@@ -2,28 +2,28 @@
     <li class="app-navigation-entry"><span class="navmarg"></span><b><?php p($l->t('Manage'));?></b></li>
 	<li class="app-navigation-entry">
 		<ul class="app-navigation">
-			<li class="app-navigation-entry"><span class="navmarg icon-contacts-dark"></span><a class="a-entry" href="<?php echo($_['url']['index']); ?>"><?php p($l->t('Customer'));?></a>
+			<li class="app-navigation-entry"><span class="navmarg icon-contacts-dark"></span><a class="a-entry" href="<?php echo($_['url']['index']); ?>"><?php p($l->t('Customers'));?></a>
 				<div class="app-navigation-entry-utils">
 					<ul>
 					<li class="app-navigation-entry-utils-counter"><span id="statsclient"><div class="loader"></div></span></li>
 					</ul>
 				</div>
 			</li>
-			<li class="app-navigation-entry"><span class="navmarg icon-template-add"></span><a href="<?php echo($_['url']['devis']); ?>"><?php p($l->t('Quote'));?></a>
+			<li class="app-navigation-entry"><span class="navmarg icon-template-add"></span><a href="<?php echo($_['url']['devis']); ?>"><?php p($l->t('Quotes'));?></a>
 				<div class="app-navigation-entry-utils">
 					<ul>
 					<li class="app-navigation-entry-utils-counter"><span id="statsdevis"><div class="loader"></div></span></li>
 					</ul>
 				</div>
 			</li>
-			<li class="app-navigation-entry"><span class="navmarg icon-toggle-pictures"></span><a href="<?php echo($_['url']['facture']); ?>"><?php p($l->t('Invoice'));?></a>
+			<li class="app-navigation-entry"><span class="navmarg icon-toggle-pictures"></span><a href="<?php echo($_['url']['facture']); ?>"><?php p($l->t('Invoices'));?></a>
 				<div class="app-navigation-entry-utils">
 					<ul>
 						<li class="app-navigation-entry-utils-counter"><span id="statsfacture"><div class="loader"></div></span></li>
 					</ul>
 				</div>	
 			</li>
-			<li class="app-navigation-entry"><span class="navmarg icon-category-integration"></span><a href="<?php echo($_['url']['produit']); ?>"><?php p($l->t('Product'));?></a>
+			<li class="app-navigation-entry"><span class="navmarg icon-category-integration"></span><a href="<?php echo($_['url']['produit']); ?>"><?php p($l->t('Products'));?></a>
 				<div class="app-navigation-entry-utils">
 					<ul>
 					<li class="app-navigation-entry-utils-counter"><span id="statsproduit"><div class="loader"></div></span></li>
@@ -32,7 +32,7 @@
 			</li>
 		</ul>
 	</li>
-	<li class="app-navigation-entry"><span class="navmarg"></span><b><?php p($l->t('Information'));?></b></li>
+	<li class="app-navigation-entry"><span class="navmarg"></span><b><?php p($l->t('Informations'));?></b></li>
 	<li class="app-navigation-entry">
 		<ul class="app-navigation">
 			<li class="app-navigation-entry"><span class="navmarg icon-toggle-pictures"></span><a href="<?php echo($_['url']['statistique']); ?>"><?php p($l->t('Statistics'));?></a></li>

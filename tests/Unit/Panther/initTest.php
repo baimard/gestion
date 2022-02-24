c0a1a698d70d2707c9ee58bc443542c55cff1896
Benjamin <benjamin@cybercorp.fr>
2022-02-24 16:45:02 +0100

    tests

--- tests/Unit/Panther/initTest.php
+++ tests/Unit/Panther/initTest.php
@@ -3,15 +3,14 @@
 use Symfony\Component\Panther\Client;
 require __DIR__.'/../../../vendor/autoload.php';
 
-// $client = Client::createFirefoxClient();
-$client = Client::createChromeClient();
-$client->request('GET', 'http://next.cybercorp.fr');
+$client = Client::createFirefoxClient();
+$client->request('GET', 'http://127.0.0.1');
 
 $client->executeScript("document.getElementById('showAdvanced').click()");
 $client->waitForVisibility('#mysql');
 $client->executeScript("document.getElementById('mysql').click()");
 
-$client->submitForm('Terminer l\'installation', [
+$client->submitForm("Finish setup", [
     'adminlogin' => 'nextcloud',
     'adminpass' => 'nextcloud',
     'dbuser' => 'nextcloud',
@@ -23,12 +22,14 @@ $client->submitForm('Terminer l\'installation', [
 
 $client = Client::createFirefoxClient();
 
-$client->request('GET', 'http://next.cybercorp.fr/login');
+$client->request('GET', 'http://127.0.0.1/login');
 $client->submitForm('submit-form', [
     'user' => 'nextcloud',
     'password' => 'nextcloud'
 ]);
+
 sleep(5);
+
 $client->executeScript("document.getElementById('firstrunwizard').getElementsByClassName('icon-close')[0].click()");
-$client->request('GET', 'http://next.cybercorp.fr/settings/apps/organization');
+$client->request('GET', 'http://127.0.0.1/settings/apps/organization');
 $client->executeScript("var els = document.getElementsByClassName('app-name');for(var i = 0; i < els.length; i++){if(els[i].innerHTML.indexOf('Gestion') > -1){els[i].parentNode.getElementsByClassName('enable')[0].click()}}");

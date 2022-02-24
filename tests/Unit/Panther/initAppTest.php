f232bb874b703edea0e7642b3982b7732927d6da
Benjamin <benjamin@cybercorp.fr>
2022-02-24 17:13:59 +0100

    test

--- tests/Unit/Panther/initAppTest.php
+++ tests/Unit/Panther/initAppTest.php
@@ -4,25 +4,6 @@ use Symfony\Component\Panther\Client;
 require __DIR__.'/../../../vendor/autoload.php';
 
 $client = Client::createFirefoxClient();
-$client->request('GET', 'http://127.0.0.1');
-
-$client->executeScript("document.getElementById('showAdvanced').click()");
-$client->waitForVisibility('#mysql');
-$client->executeScript("document.getElementById('mysql').click()");
-
-$client->submitForm("Finish setup", [
-    'adminlogin' => 'nextcloud',
-    'adminpass' => 'nextcloud',
-    'dbuser' => 'nextcloud',
-    'dbpass' => 'nextcloud',
-    'dbname' => 'nextcloud',
-    'dbhost' => 'database',
-    'dbtype' => 'mysql'
-]);
-
-sleep(5);
-
-$client = Client::createFirefoxClient();
 
 $client->request('GET', 'http://127.0.0.1/login');
 $client->submitForm('submit-form', [

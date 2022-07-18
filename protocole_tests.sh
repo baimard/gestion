a9c79a150dbcaabee84c2be89569794e8f2fd253
Benjamin <benjamin@cybercorp.fr>
2022-07-18 22:50:06 +0200

    Protocole tests

--- protocole_tests.sh
+++ protocole_tests.sh
@@ -9,18 +9,18 @@ docker run -d --rm --network next --name database -p 3306:3306 -e MYSQL_DATABASE
 
 sleep 5
 echo "Start nextcloud"
-docker run -d --rm --network next --name nextcloud -p 80:80 nextcloud:23-apache
+docker run -d --rm --network next --name nextcloud -p 80:80 nextcloud:24-apache
 
 echo "Installation"
-docker exec -it nextcloud bash -c "apt update ; apt install -y git make nodejs npm firefox-esr unzip"
-#docker exec -it nextcloud bash -c "cd /tmp; wget https://github.com/mozilla/geckodriver/releases/download/v0.30.0/geckodriver-v0.30.0-linux64.tar.gz ; tar xvzf geckodriver-v0.30.0-linux64.tar.gz -C /tmp/ ;  chown -R root:root /tmp/geckodriver* ; mv /tmp/geckodriver* /opt/ ; ln -s /opt/geckodriver/geckodriver /usr/local/bin/geckodriver"
+docker exec -it nextcloud bash -c "apt update ; apt install -y git make nodejs npm firefox-esr unzip wget"
+# docker exec -it nextcloud bash -c "cd /tmp; wget https://github.com/mozilla/geckodriver/releases/download/#v0.30.0/geckodriver-v0.30.0-linux64.tar.gz ; tar xvzf geckodriver-v0.30.0-linux64.tar.gz -C /tmp/ ;  chown -R root:root /tmp/geckodriver* ; mv /tmp/geckodriver* /opt/ ; ln -s /opt/geckodriver/geckodriver /usr/local/bin/geckodriver"
 
-docker exec -it nextcloud bash -c "git clone https://github.com/baimard/gestion.git /var/www/html/apps/gestion ; cd /var/www/html/apps/gestion ; git checkout dev-2.1.0 ; chown www-data:root -R /var/www/html/apps/gestion"
+docker exec -it nextcloud bash -c "git clone https://github.com/baimard/gestion.git /var/www/html/apps/gestion ; cd /var/www/html/apps/gestion ; git checkout dev-2.1.2 ; chown www-data:root -R /var/www/html/apps/gestion"
 docker exec -u www-data -it nextcloud bash -c "cd apps/gestion ; make npm-init ; make composer;"
 
 echo "Initialisation de la base de données"
 docker exec -u www-data -it nextcloud bash -c "cd apps/gestion ; php tests/Unit/Panther/initMysqlTest.php"
-# docker exec -u www-data -it nextcloud bash -c "cd apps/gestion ; php tests/Unit/Panther/initPgsqlTest.php"
+docker exec -u www-data -it nextcloud bash -c "cd apps/gestion ; php tests/Unit/Panther/initPgsqlTest.php"
 
 sleep 10
 

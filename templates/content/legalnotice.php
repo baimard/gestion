815520fbc5e3f9380dacba10e6121770bfb51d62
Valdnet <47037905+Valdnet@users.noreply.github.com>
2022-08-19 15:41:00 +0200

    l10n: Reconstruction of sentence

--- templates/content/legalnotice.php
+++ templates/content/legalnotice.php
@@ -9,7 +9,7 @@
         </div>
     </div>
     <h2><?php p($l->t('Legal notice')); ?></h2>
-    <p><?php p($l->t('This section provides information pertaining to the use of this application under the legal framework applicable in various countries.')); ?></p>
+    <p><?php p($l->t('This section provides information on the use of this application in accordance with the laws in various countries.')); ?></p>
     <p><?php p($l->t('If you want to contribute to this section just contact me by mail')); ?> <b><a style="text-decoration:underline" href="mailto:benjamin@cybercorp.fr">Benjamin A.</a></b></p>
     <hr/>
     <ul style="margin-left:30px;">

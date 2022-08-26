22901a697b41c6e52163e6873781c6f263ff162a
Benjamin <benjamin@cybercorp.fr>
2022-08-26 15:31:12 +0200

    Dev 2.1.7 (#278)
    * ...
    
    * Legal notice 2.1.7
    
    * Transifex
    
    * Newsletter

--- templates/content/legalnotice.php
+++ templates/content/legalnotice.php
@@ -9,8 +9,8 @@
         </div>
     </div>
     <h2><?php p($l->t('Legal notice')); ?></h2>
-    <p><?php p($l->t('This section provides information on the use of this application in accordance with the laws in various countries.')); ?></p>
-    <p><?php p($l->t('If you want to contribute to this section just contact me by email')); ?> <b><a style="text-decoration:underline" href="mailto:benjamin@cybercorp.fr">Benjamin A.</a></b></p>
+    <p><?php p($l->t('This section provides information pertaining to the use of this application under the legal framework applicable in various countries.')); ?></p>
+    <p><?php p($l->t('If you want to contribute to this section just contact me by mail')); ?> <b><a style="text-decoration:underline" href="mailto:benjamin@cybercorp.fr">Benjamin A.</a></b></p>
     <hr/>
     <ul style="margin-left:30px;">
         <li>🇫🇷 <a href="<?php echo $_['url']['france']; ?>" title="Cliquer ici pour voir la notice" style="font-size:20px;">France</a> par <b>Timo RAINIO</b> Avocat <a title="Droit des affaires et de l’entreprise – Droit du travail – Droit du numérique. Vous pouvez retrouver nos domaines d’intervention sur notre site internet" style="text-decoration:underline" href="https://www.avocat-rainio.com">Cabinet d’Avocats Rainio</a> - mise à jour 16 mai 2022</li>

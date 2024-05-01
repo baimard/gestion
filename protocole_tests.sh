f78835e4b2b805f09b32264ee2f4a08510929237
Benjamin AIMARD <benjamin@cybercorp.fr>
2024-05-01 17:32:48 +0200

    2.5.1

--- protocole_tests.sh
+++ protocole_tests.sh
@@ -10,7 +10,7 @@ sudo docker run -d --rm --network next --name database -p 3306:3306 -e MARIADB_D
 sleep 5
 
 echo "Start nextcloud"
-sudo docker run -d --rm --network next --name nextcloud -p 80:80 nextcloud:28-apache
+sudo docker run -d --rm --network next --name nextcloud -p 80:80 nextcloud:29-apache
 
 echo "Installation"
 sudo docker exec -it nextcloud bash -c "apt update ; apt install -y git make nodejs npm firefox-esr unzip wget"

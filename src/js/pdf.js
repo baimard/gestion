514bcc5225071df03536b3f4f9b61996d5ad3a84
rakekniven <2069590+rakekniven@users.noreply.github.com>
2025-06-03 12:27:06 +0200

    chore(i18n): Changed source strings to english (#452)

--- src/js/pdf.js
+++ src/js/pdf.js
@@ -69,10 +69,10 @@ export function capture(afterCapturefunction) {
     a.click();
     window.URL.revokeObjectURL(url);
   
-    showMessage(t("gestion", "PDF créé avec succès."));
+    showMessage(t("gestion", "PDF has been created."));
   })
   .catch(error => {
-    console.error("Erreur lors de la génération du PDF :", error);
-    showMessage(t("gestion", "Erreur lors de la création du PDF."));
+    console.error("Errors during PDF generation :", error);
+    showMessage(t("gestion", "Error when creating PDF."));
   });
 }

bf8e05297477e477236568a85c0ba97b2d614356
Benjamin <benjamin@cybercorp.fr>
2026-08-06 15:52:07 +0200

    Align VAT category header

--- templates/content/produit.php
+++ templates/content/produit.php
@@ -13,12 +13,10 @@
                 <th><?php p($l->t('Designation'));?></th>
                 <th><?php p($l->t('Unit price without VAT'));?></th>
                 <th><?php p($l->t('VAT percentage'));?></th>
-                <th>
-					<?php p($l->t('VAT category (France only)'));?>
-					<button type="button" id="vatCategoryHelp" class="vat-category-help" aria-label="<?php p($l->t('Explain VAT categories'));?>" title="<?php p($l->t('Explain VAT categories'));?>">
-						<span class="vat-category-help-icon" aria-hidden="true">!</span>
-					</button>
-				</th>
+	            <th>
+	                <span><?php p($l->t('VAT category (France only)'));?></span>
+	                <span id="vatCategoryHelp" class="vat-category-help" role="button" tabindex="0" aria-label="<?php p($l->t('Explain VAT categories'));?>" title="<?php p($l->t('Explain VAT categories'));?>">!</span>
+	            </th>
                 <th><?php p($l->t('Header'));?></th>
                 <th><?php p($l->t('Actions'));?></th>
             </tr>
